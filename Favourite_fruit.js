791e2e64a2fb04d0ceaf9c76a523f2954e4e1dea
Tayyaba Amjad Rao <160313142+TayyabaAmjadRao@users.noreply.github.com>
2024-02-19 13:30:53 -0800

    Add files via upload

---
+++ Favourite_fruit.js
@@ -0,0 +1,19 @@
+"use strict";
+// Written by : TAYYABA RAO
+//Date: 18/feb/2024
+const favourite_fruit = ["banana", "apple", "mango"];
+if (favourite_fruit.includes("banana")) {
+    console.log("I like bananas!");
+}
+if (favourite_fruit.includes("apple")) {
+    console.log("I like apples!");
+}
+if (favourite_fruit.includes("mango")) {
+    console.log("I like mangoes!");
+}
+if (favourite_fruit.includes("orange")) {
+    console.log("I like oranges!");
+}
+if (favourite_fruit.includes("kiwi")) {
+    console.log("I like kiwis!");
+}
